0d8dd200bce75f5d5c9592b7b17c97d4b681008c
jgazor-debug <jgazor@gbox.adnu.edu.ph>
2026-02-18 23:41:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    const message = document.getElementById("messageBox");
+    message.textContent = "try ko lang po magbutton ma'am :>";
+    message.classList.add("show");
+}
+
