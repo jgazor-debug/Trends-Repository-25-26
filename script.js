3c1ebfbd940cc9e318d8880e2cd5649d5efd0d38
jgazor-debug <jgazor@gbox.adnu.edu.ph>
2026-02-18 23:43:43 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showMessage() {
-    const message = document.getElementById("messageBox");
-    message.textContent = "try ko lang po magbutton ma'am :>";
-    message.classList.add("show");
-}
-
